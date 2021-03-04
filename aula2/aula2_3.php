31db1b0b7fc643827e8211a376415f1714629595
Hebertphp <hebertphp@gmail.com>
2021-03-04 10:17:57 -0300

    Update aula2_3.php

--- aula2/aula2_3.php
+++ aula2/aula2_3.php
@@ -1,15 +1,11 @@
-  <head>
-    <title>Aula2_3</title>
-  </head>
-  <body>
+<html>
+<head>
+  <title>Aula2_3</title>
+</head>
+<body>
   <?php
-    $_nome="João Antonio";
-    $Nome="Alfredo Maciel";
-    $valor=123456;
     $nota=7.5;
-    var_dump($Nome);
-    var_dump($valor);
     var_dump($nota);
   ?>
-  </body>
+</body>
 </html>
