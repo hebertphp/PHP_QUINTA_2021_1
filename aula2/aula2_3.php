ada70846b106a6054e137066ec9918043a6b08bc
Hebertphp <hebertphp@gmail.com>
2021-03-04 10:12:40 -0300

    Create aula2_3.php

---
+++ aula2/aula2_3.php
@@ -0,0 +1,15 @@
+  <head>
+    <title>Aula2_3</title>
+  </head>
+  <body>
+  <?php
+    $_nome="João Antonio";
+    $Nome="Alfredo Maciel";
+    $valor=123456;
+    $nota=7.5;
+    var_dump($Nome);
+    var_dump($valor);
+    var_dump($nota);
+  ?>
+  </body>
+</html>
